5b2250fddf2b7437d2cfc2d058d7e567df4ab085
pankaj-gitcode <pankajaditya75@gmail.com>
2024-07-27 02:14:15 +0530

    2nd commit ++comments

--- JS_scripts/1fileReadWrite.js
+++ JS_scripts/1fileReadWrite.js
@@ -1,19 +1,21 @@
+// ----------------------------------------------------------------------------------------------------------------------------------------- //
 // Below script will fetch data/files from particular directory and write those data/files to another file:
-// say /dirImages/1.png 2.png....100.png files,so we've 100 images and wanted to fetch & write only name of file in separate file say image.txt
-//Line No:-10 & 14 need to be changed as per your path
+// say /dirImages/1.png 2.png....100.png files,so we've 100 images and wanted to fetch & write only name of file in separate file say output.txt
+// Line Nos. with 🚩 need to be changed as per your path
 // save this nodeJS file and run: >node fileName.js
 // ----------------------------------------------------------------------------------------------------------------------------------------- //
+
 
 //import the 'file System' & 'path'
 const fs = require('fs');
 const path = require('path');
 
 // join the directory with file name compatible in both Unix & windows env.
-const readFrom = '../../Projects/FullStack_Projects/Food_delivery_assets/frontend_assets' //paste your file from you wanted to read/fetch from
+const readFrom = '../../Projects/FullStack_Projects/Food_delivery_assets/frontend_assets' //🚩paste your file from you wanted to read/fetch from
 const dirName = path.join(__dirname, readFrom);
 
 //files to be written
-const writeTo = '../../Projects/FullStack_Projects/Food_delivery_assets/outputFile/allFileNames.txt'; //where you need to write file to
+const writeTo = '../../Projects/FullStack_Projects/Food_delivery_assets/outputFile/allFileNames.txt'; //🚩where you need to write file to
 const outputFiles = path.join(__dirname, writeTo);
 
 //read & write the file
@@ -21,16 +23,17 @@ const readWriteFile = ()=>{
     try{
 
         return new Promise((resolve, reject)=>{
-            //read file
-            fs.readdir(dirName, (err, files)=>{  //'files' consists all .png file names in array
+            //read files
+            fs.readdir(dirName, (err, files)=>{  //'files' consist all .png file names in array
                 if(err){console.error("ERROR=> ",err.message)};
                 resolve(files)  
             })
         })
         .then(allFiles=>{
-            console.log(allFiles);
+            console.log("All files: ",allFiles);
             const filesContent = allFiles.join(`\n`)
             
+            //write files
             fs.writeFile(outputFiles, filesContent, (err)=>{
                 if(err){console.error(`ERROR=> ${err}`); return;};
                 console.log("Output File: ",outputFiles);
